9cf109b12a907464ad173f1e7545a38898b9fffd
duoleyisui <1694474987@qq.com>
2019-10-18 16:19:54 +0800

    1

---
+++ perfect/start/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+     <router-link to="/about">登录</router-link>
+     <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
